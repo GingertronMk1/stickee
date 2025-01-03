a05a9b31fbf276ff04a44e71885b874bad2e550f
Jack Ellis <jackellis1504@gmail.com>
2025-01-03 20:22:24 +0000

    Add test cases as per the spec document

--- tests/Unit/WidgetCounterTest.php
+++ tests/Unit/WidgetCounterTest.php
@@ -1,5 +1,23 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
-});
+$counter = new \App\Services\WidgetCounter([
+    250,500,1000,2000,5000
+]);
+
+$results = [
+    1 => [ 250 => 1 ],
+    250 => [ 250 => 1 ],
+    251 => [ 500 => 1 ],
+    500 => [ 500 => 1 ],
+    501 => [ 500 => 1, 250 => 1 ],
+    12001 => [ 5000 => 2, 2000 => 1, 250 => 1 ],
+];
+
+foreach ($results as $widgets => $output) {
+    test(
+        "Test for {$widgets} widgets",
+        function () use ($counter, $widgets, $output) {
+            expect($counter->getWidgetPacks($widgets))->toBe($output);
+        }
+    );
+}

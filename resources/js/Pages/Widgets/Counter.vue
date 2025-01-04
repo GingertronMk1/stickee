3c515e19c630c87ad40fb90047c2aa6c113d1a2d
Jack Ellis <jackellis1504@gmail.com>
2025-01-04 15:46:25 +0000

    UX updates

--- resources/js/Pages/Widgets/Counter.vue
+++ resources/js/Pages/Widgets/Counter.vue
@@ -9,32 +9,48 @@ interface PackCalculation {
 }
 
 const props = defineProps<{
-    widgets: integer | null,
+    widgets?: number,
     packs: PackCalculation,
     lookedUp: boolean,
 }>();
 
-const form = useForm<{widgets: integer|null}>({widgets: props.widgets});
+const form = useForm<{ widgets?: number }>({widgets: props.widgets});
 const submit = () => form.get('/');
 
 </script>
 
 <template>
-    <span>This value {{ lookedUp ? 'was' : 'was not' }} retrieved from the database</span>
-    <input v-model="form.widgets" />
-    <button @click="submit">Calculate</button>
-    <table>
-        <thead>
-        <tr>
-            <th>Pack Size</th>
-            <th>Count</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="(amount, packSize) in packs.packs" :key="packSize">
-            <td v-text="packSize" />
-            <td v-text="amount" />
-        </tr>
-        </tbody>
-    </table>
+    <header class="p-6 bg-blue-700 text-white">
+        <h1 class="text-3xl">Welcome to Wally's Widgets!</h1>
+    </header>
+    <main class="py-3">
+        <div class="w-11/12 max-w-screen-xl mx-auto">
+            <div class="border-2 border-gray-500 rounded-lg p-4 mb-3">
+                <label for="widget_count" class="flex flex-col mb-3">
+                    Please enter the desired number of Widgets
+                    <input name="widget_count" v-model="form.widgets" type="number" min="0" class="rounded-md"/>
+                </label>
+                <button @click="submit" class="bg-blue-700 rounded-md text-white p-2">Calculate</button>
+            </div>
+
+           <template v-if="packs.widget_count > 0" >
+            <table class="border-collapse border border-slate-500 w-full">
+                <thead>
+                <tr>
+                    <th class="border border-slate-600 p-3">Pack Size</th>
+                    <th class="border border-slate-600 p-3">Count</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(amount, packSize) in packs.packs" :key="packSize">
+                    <td class="border border-slate-700 p-3" v-text="packSize"/>
+                    <td class="border border-slate-700 p-3" v-text="amount"/>
+                </tr>
+                </tbody>
+            </table>
+
+            <span >This value {{ lookedUp ? 'was' : 'was not' }} retrieved from the database</span>
+           </template>
+        </div>
+    </main>
 </template>
